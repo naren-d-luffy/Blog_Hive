6fa3550cc6597b9d2776743b73f68df18d19988d
Naren-d-luffy <deadlyassassin2003@gmail.com>
2026-03-23 17:07:40 +0530

    Adding centralized Error handler

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,36 @@ import app from "./app";
 import env from "./config/env.config";
 import connectDB from "./config/db.config";
 
-const start =  async():Promise<void> =>{
-    try {
-        await connectDB();
+let server: any;
 
-        app.listen(env.PORT, ()=>{
-            console.log(`Server is running on http:localhost:${env.PORT}`);
-            
-        })
-    } catch (error) {
-        console.error("Server failed to start",error);
-        process.exit(1);
-    }
-}
+const start = async (): Promise<void> => {
+  try {
+    await connectDB();
+
+    app.listen(env.PORT, () => {
+      console.log(`Server is running on http:localhost:${env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Server failed to start", error);
+    process.exit(1);
+  }
+};
+
+process.on("uncaughtException", (err: Error) => {
+  console.error("UNCAUGHT EXCEPTION", err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err: Error) => {
+  console.error("UNCAUGHT REJECTION", err);
+  if (server) {
+    server.close(() => {
+      console.log("Server Closed gracefully");
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
 
 start();

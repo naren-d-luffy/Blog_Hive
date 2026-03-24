984e3c2783f0bd29fd0a19b2d173143ca481e053
Naren-d-luffy <deadlyassassin2003@gmail.com>
2026-03-24 17:36:22 +0530

    making the status code in the App error dynamic

--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -2,7 +2,7 @@ export default class AppError extends Error{
     statusCode: number;
     isOperational: boolean;
 
-    constructor(message:string,statusCode:500){
+    constructor(message:string,statusCode:number){
         super(message);
         this.statusCode = statusCode;
         this.isOperational = true;

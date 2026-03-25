46fbacb2d449b0c8ce5e78857f25845152548c25
Naren-d-luffy <deadlyassassin2003@gmail.com>
2026-03-25 12:07:34 +0530

    Adding Authenticate and Role Authorize functions in Auth Middleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import env from "../config/env.config";
+import AppError from "../utils/AppError";
+import { AuthUser } from "../types/auth.types";
+
+const ACCESS_SECRET = env.ACCESS_TOKEN;
+
+const Authenticate = (req: Request,res: Response,next: NextFunction,): void => {
+  const auth = req.headers.authorization;
+
+  if (!auth || !auth.startsWith("Bearer ")) {
+    return next(new AppError("unauthorized: No token provided", 401));
+  }
+
+  const token = auth.split(" ")[1];
+
+  try {
+    const decode = jwt.verify(token, ACCESS_SECRET) as AuthUser;
+    req.user = decode;
+
+    next();
+  } catch (error) {
+    return next(
+      new AppError("Unauthorized: Invalid or expired token", 401, { error }),
+    );
+  }
+};
+
+const Authorize = (...role: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user) {
+      return next(new AppError("Unauthorized", 401));
+    }
+
+    if (!role.includes(req.user.role)) {
+      return next(new AppError("Forbidden", 403));
+    }
+
+    next();
+  };
+};
+
+export default { Authenticate, Authorize };

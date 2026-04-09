d3d2a73b8d964cce109a61b15c9fd6055cbafe45
Naren-d-luffy <deadlyassassin2003@gmail.com>
2026-04-09 11:28:34 +0530

    creating email config and adding new env for email config

---
+++ src/config/config/email.config.ts
@@ -0,0 +1,12 @@
+import nodemailer from 'nodemailer'
+import env from '../env.config';
+
+export const transporter = nodemailer.createTransport({
+    host: env.SMTP_HOST,
+    port: env.SMTP_PORT,
+    secure: env.SMTP_PORT.toString() === "465",
+    auth: {
+        user: env.SMTP_USER,
+        pass: env.SMTP_PASS
+    }
+})
